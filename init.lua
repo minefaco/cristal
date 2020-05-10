c7688fad6aaa81aada7dc44a44ec28cbc6e4fec8
minefaco <63024226+minefaco@users.noreply.github.com>
2020-05-10 14:30:54 -0500

    Inicializar todos los archivos

---
+++ init.lua
@@ -0,0 +1,7 @@
+cristal = {}
+
+cristal.modpath = minetest.get_modpath("cristal")
+
+dofile(cristal.modpath.."/cristal.lua")
+dofile(cristal.modpath.."/panes.lua")
+dofile(cristal.modpath.."/crafts.lua")
